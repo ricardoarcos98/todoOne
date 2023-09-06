c5f241c3b6c488869e17825587b5f61ef3bd6659
RicardoArcos1 <ricardoarcos98@hotmail.com>
2023-09-06 15:08:18 -0500

    TODO DARKMODE TRANSITION1

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,7 @@ import TodoItem from "./TodoItem";
 
 const TodoList = ({ todo, removeTodo, updateTodo }) => {
     return (
-        <div className="bg-white rounded-t-md [&>article]:p-4 mt-8 overflow-hidden transition-all duration-1000 dark:bg-gray-800 ">
+        <div className="bg-white rounded-t-md [&>article]:p-4 mt-8 overflow-hidden dark:bg-gray-800  transition-all duration-1000 ">
             {todo.map((todo) => (
                 <TodoItem
                     key={todo.id}
